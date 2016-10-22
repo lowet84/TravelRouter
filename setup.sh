08b79c37e33046e90d105ed049cf45399bb5107d
Fredrik Löwenhamn <lowet84@gmail.com>
2016-10-22 15:21:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y make isc-dhcp-server
+apt-get install -y make isc-dhcp-server vim
 wget https://github.com/jenssegers/RTL8188-hostapd/archive/v2.0.tar.gz
 tar xvf v2.0.tar.gz
 cd RTL8188-hostapd-2.0/hostapd/
