132d40e5eef4505bcdf3d0d06045b9351db62c5c
Fredrik Löwenhamn <lowet84@gmail.com>
2016-10-22 15:26:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ wget https://github.com/jenssegers/RTL8188-hostapd/archive/v2.0.tar.gz
 tar xvf v2.0.tar.gz
 cd RTL8188-hostapd-2.0/hostapd/
 make && make install
+wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/dhcpd.conf -O /etc/dhcp/dhcpd.conf
