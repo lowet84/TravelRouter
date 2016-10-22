7b7b3f9396b8024632ea8e3ea8ade0621d384c9e
Fredrik Löwenhamn <lowet84@gmail.com>
2016-10-22 15:49:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+WLAN=$1
 apt-get update
 apt-get install -y make isc-dhcp-server vim
 wget https://github.com/jenssegers/RTL8188-hostapd/archive/v2.0.tar.gz
@@ -5,14 +6,14 @@ tar xvf v2.0.tar.gz
 cd RTL8188-hostapd-2.0/hostapd/
 make && make install
 wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/dhcpd.conf -O /etc/dhcp/dhcpd.conf
-echo "INTERFACES=\"wlan0\"" > /etc/default/isc-dhcp-server
-wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/wlan0 -O /etc/network/interfaces.d/wlan0
+echo "INTERFACES=\"$WLAN\"" > /etc/default/isc-dhcp-server
+wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/$WLAN -O /etc/network/interfaces.d/$WLAN
 wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/hostapd.conf -O /etc/hostapd/hostapd.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-ifup wlan0
+ifup $WLAN
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o $WLAN -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $WLAN -o eth0 -j ACCEPT
 service isc-dhcp-server start
 hostapd -B /etc/hostapd/hostapd.conf
