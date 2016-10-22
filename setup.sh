843410799160644e3e0eed216266d70e27fe4244
Fredrik Löwenhamn <lowet84@gmail.com>
2016-10-22 15:45:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ echo "INTERFACES=\"wlan0\"" > /etc/default/isc-dhcp-server
 wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/wlan0 -O /etc/network/interfaces.d/wlan0
 wget https://raw.githubusercontent.com/lowet84/TravelRouter/master/hostapd.conf -O /etc/hostapd/hostapd.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+ifup wlan0
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+service isc-dhcp-server start
+hostapd -B /etc/hostapd/hostapd.conf
